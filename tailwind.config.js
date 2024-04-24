2ad86dd7df9c44601880699fcbdb4cd0946f34b3
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2024-04-24 17:42:06 +0700

    added-extends: fonts Poppins and Noto Sans Thai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: ['selector', '[data-mode="dark"]'],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'eng': ['Poppins', 'sans-serif'],
+        'thai': ['"Noto Sans Thai"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
