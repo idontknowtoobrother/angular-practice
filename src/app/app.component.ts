8cc5b81dd02bf3e764d715266053024ec574b92a
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2024-04-24 16:41:51 +0700

    test-install: TailwindCSS and setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HeroDetailComponent } from './components/hero-detail.component';
   standalone: true,
   imports: [RouterOutlet, HeroListComponent, HeroDetailComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'regiset-form-tailwindcss';
